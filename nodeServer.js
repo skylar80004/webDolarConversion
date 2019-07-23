3965f8e4d2b8c45037265ceb4efcd2baec59cbef
skylar80004 <josedavidvm04@hotmail.com>
2019-07-22 21:34:17 -0600

    Project Files

---
+++ nodeServer.js
@@ -0,0 +1,14 @@
+
+
+var express = require('express');
+var app = express();
+app.set("view engine" , "ejs");
+
+app.get('/',function(request,response){
+
+  response.render("index");
+
+});
+
+app.listen(3000);
+console.log("Listening to port");
